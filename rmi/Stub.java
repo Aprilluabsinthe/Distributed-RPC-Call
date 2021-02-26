c2203fc35ac849aba4ac34add08912d195789513
Di Lu <dilu@andrew.cmu.edu>
2021-02-26 00:11:09 -0500

    realize three construction functions
    How to override equals/hashcode/tostring in an abstract class?

--- rmi/Stub.java
+++ rmi/Stub.java
@@ -1,7 +1,13 @@
 package rmi;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.List;
+import rmi.Helper.Helper;
 
-import java.net.*;
-import java.rmi.Remote
+import static rmi.Helper.Helper.allThrowRMIExceptions;
 
 /** RMI stub factory.
 
@@ -16,8 +22,10 @@ import java.rmi.Remote
     same interface and carry the same remote server address - and would
     therefore connect to the same skeleton. Stubs are serializable.
  */
-public abstract class Stub
+public abstract class Stub<T>
 {
+    private static Class<T> c;
+    private static Skeleton<T> skeleton;
     /** Creates a stub, given a skeleton with an assigned adress.
 
         <p>
@@ -46,11 +54,29 @@ public abstract class Stub
                       <code>RMIException</code>, or if an object implementing
                       this interface cannot be dynamically created.
      */
+
     public static <T> T create(Class<T> c, Skeleton<T> skeleton)
         throws UnknownHostException
     {
-        // TODO:
-        throw new UnsupportedOperationException("not implemented");
+        if(skeleton.getAddress() == null){
+            throw new IllegalStateException("keleton has not been assigned an address by the user and has not yet been started.");
+        }
+        if(c == null || skeleton == null){
+            throw new NullPointerException("interface or server is null");
+        }
+        if( !c.isInterface() ){
+            throw new Error("interface is not remote");
+        }
+        // if every method throws RMIExeption
+
+        if (!allThrowRMIExceptions(c)){
+            throw new Error("Methods do not throw RMIException");
+        }
+
+        proxyInvocationHandler stubHandler = new proxyInvocationHandler(skeleton.getAddress());
+        c = c;
+        skeleton = skeleton;
+        return (T) Proxy.newProxyInstance(skeleton.getClassT().getClassLoader(), skeleton.getServer().getClass().getInterfaces(),stubHandler);
     }
 
     /** Creates a stub, given a skeleton with an assigned address and a hostname
@@ -86,40 +112,92 @@ public abstract class Stub
     public static <T> T create(Class<T> c, Skeleton<T> skeleton,
                                String hostname)
     {
-        // TODO:
-        throw new UnsupportedOperationException("not implemented");
+        try{
+            if(skeleton.getAddress() == null){
+                throw new IllegalStateException("skeleton has not been assigned an address by the user and has not yet been started.");
+            }
+            if(c == null || skeleton == null || hostname == null){
+                throw new NullPointerException("interface/server/host is null");
+            }
+            if( !c.isInterface() ){
+                throw new Error("interface is not remote");
+            }
+            // if every method throws RMIExeption
+            if (!allThrowRMIExceptions(c)){
+                throw new Error("Methods do not throw RMIException");
+            }
+
+            skeleton.setAddress(hostname , skeleton.getPort());
+            proxyInvocationHandler stubHandler = new proxyInvocationHandler(skeleton.getAddress());
+            return (T) Proxy.newProxyInstance(skeleton.getClassT().getClassLoader(), skeleton.getServer().getClass().getInterfaces(),stubHandler);
+        } catch (UnsupportedOperationException e){
+            throw new UnsupportedOperationException("not implemented");
+        }
     }
 
     /** Creates a stub, given the address of a remote server.
 
-        <p>
-        This method should be used primarily when bootstrapping RMI. In this
-        case, the server is already running on a remote host but there is
-        not necessarily a direct way to obtain an associated stub.
+     <p>
+     This method should be used primarily when bootstrapping RMI. In this
+     case, the server is already running on a remote host but there is
+     not necessarily a direct way to obtain an associated stub.
 
-        @param c A <code>Class</code> object representing the interface
-                 implemented by the remote object.
-        @param address The network address of the remote skeleton.
-        @return The stub created.
-        @throws NullPointerException If any argument is <code>null</code>.
-        @throws Error If <code>c</code> does not represent a remote interface
-                      - an interface in which each method is marked as throwing
-                      <code>RMIException</code>, or if an object implementing
-                      this interface cannot be dynamically created.
+     @param c A <code>Class Interface</code> to be compared to
+     @param address A <code>InetSocketAddress</code> to be compared to
+     @return True if equals, False if not
      */
-    public static boolean equals(<T> T other)
-    {
-        // TODO:
+    public static <T> T create(Class<T> c, InetSocketAddress address){
+        if(c == null || address == null){
+            throw new NullPointerException("interface/server/host is null");
+        }
+        if( !c.isInterface() ){
+            throw new Error("interface is not remote");
+        }
+        // if every method throws RMIExeption
+        if (!allThrowRMIExceptions(c)){
+            throw new Error("Methods do not throw RMIException");
+        }
+
+        Skeleton skeleton = new Skeleton(InetSocketAddress address);
+        proxyInvocationHandler stubHandler = new proxyInvocationHandler(skeleton.getAddress());
+        return (T) Proxy.newProxyInstance(skeleton.getClassT().getClassLoader(), skeleton.getServer().getClass().getInterfaces(),stubHandler);
+    }
+
+    /** override equals method
+        @param obj A <code>Object</code> to be compared to
+        @return True if equals, False if not
+     */
+    @Override
+    public boolean equals( Object obj) {
+        if ( obj == null ) return false;
+        if(obj instanceof Stub){
+            return ((Stub<?>) obj).getC() == c && ((Stub<?>) obj).getSkeleton() == skeleton;
+        }
+        return false;
     }
 
     @Override
-    public static int hashCode()
+    public int hashCode()
     {
-        // TODO:
+        int res = 17;
+        res = res * 31 + c.hashCode();
+        res = res * 31 + skeleton.hashCode();
+        return res;
     }
 
-    public static String toString()
+    @Override
+    public String toString()
     {
-        // TODO:
+        return c.getClass().getInterfaces() + skeleton.getAddress().getHostName() + skeleton.getAddress().getHostName();
+    }
+
+    public Class<T> getC() {
+        return c;
+    }
+
+    public Skeleton<T> getSkeleton() {
+        return skeleton;
     }
 }
+
+
