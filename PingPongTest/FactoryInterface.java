4cfd6a4a94e0b5d1705df101ac8d75c2b2da1e79
Di Lu <54696486+Aprilluabsinthe@users.noreply.github.com>
2021-03-01 02:36:38 -0500

    Delete FactoryInterface.java

--- PingPongTest/FactoryInterface.java
+++
@@ -1,9 +0,0 @@
-package PingPongTest;
-
-import rmi.RMIException;
-
-import java.net.UnknownHostException;
-
-public interface FactoryInterface {
-    public PingServer makePingServer() throws RMIException, UnknownHostException;
-}
